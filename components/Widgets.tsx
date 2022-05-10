2630bccce7aa30e6d992333e91c1f2a1f28f64a1
minguk <minkuk90@gmail.com>
2022-05-10 17:17:53 +0900

    Widgets components

---
+++ components/Widgets.tsx
@@ -0,0 +1,22 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+function Widgets() {
+  return (
+    <div className="px-2 mt-2">
+      {/* Search */}
+      <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400"/>
+        <input type="text" placeholder="무슨 일이 일어나고 있나요?" className="flex-1 outline-none bg-transparent "/>
+      </div>
+      <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="Sonny7"
+  options={{height: 400}}
+/>
+    </div>
+  )
+}
+
+export default Widgets
