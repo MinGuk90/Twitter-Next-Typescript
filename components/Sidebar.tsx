d98444cc23c2bc9d733e243e7803d31279d17331
minguk <minkuk90@gmail.com>
2022-05-11 17:45:35 +0900

    반응형 디자인

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,8 +12,8 @@ import SidebarRow from './SidebarRow'
 
 function Sidebar() {
   return (
-    <div className="flex flex-col">
-      <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
+    <div className="flex col-span-2 flex-col items-center px-4 md:items-start">
+      <img className="m-3 h-10 w-10" src="https://links.papareact.com/drq" alt="" />
       <SidebarRow Icon={HomeIcon} title="홈" />
       <SidebarRow Icon={HashtagIcon} title="탐색하기" />
       <SidebarRow Icon={BellIcon} title="알림" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Twitter</title>
       </Head>
 
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
 
         <Feed />
